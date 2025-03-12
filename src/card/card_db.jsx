68f56feac868a564a3ee5747d2d83cabe1255b07
ptozax <38245998+ptozax@users.noreply.github.com>
2025-03-13 01:03:01 +0700

    add card page

---
+++ src/card/card_db.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import thtext from "./db/cards.json";
+
+function CardList() {
+
+
+
+    return (
+        <>
+<h1>Test</h1>
+
+        </>)
+
+}
+
+export default CardList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App/App.jsx'
-import R2t from './raw_to_ydk/r2t.jsx'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App/App.jsx';
+import R2t from './raw_to_ydk/r2t.jsx';
+import Card_list from './card/card_db.jsx';
+
+const pageComponents = {
+  Cards: <Card_list />,
+  Default: <R2t />,
+};
+
+function HandlePage() {
+  const { pageId } = useParams();
+  return pageComponents[pageId] || pageComponents.Default;
+}
+
+function Navigation() {
+  return (
+    <nav>
+      <Link to="/Raw2YDK/">Home</Link> | <Link to="/Raw2YDK/Cards">Cards Info</Link>
+    </nav>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <R2t />
-  </StrictMode>,
-)
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/Raw2YDK/" element={<R2t />} />
+        <Route path="/Raw2YDK/:pageId" element={<HandlePage />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
