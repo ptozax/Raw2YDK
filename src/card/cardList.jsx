dd638b5985b3b891d70be304c8d19109bdb0cdc2
ptozax <38245998+ptozax@users.noreply.github.com>
2025-03-13 16:24:01 +0700

    re write to bootstrap

--- src/card/cardList.jsx
+++ src/card/cardList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import thtext from "./db/cards.json";
 import { useGlobalContext } from "../global_context.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,21 +6,21 @@ import './cardList.css';
 
 function CardList() {
         const { Cards, setCards } = useGlobalContext();
-
-
         const [selectedCard, setSelectedCard] = useState(null);
         const [searchQuery, setSearchQuery] = useState('');
         const [selectedType, setSelectedType] = useState('All');
+  
+
 
         const handleCardSelect = (card) => {
                 setSelectedCard(card);
         };
 
-        const shuffledCards = shuffleArray([...Cards]);
+     
 
-        const filteredCards = shuffledCards.filter((card) => {
+        const filteredCards = Cards.filter((card) => {
                 const matchesSearch = card.name.toLowerCase().includes(searchQuery.toLowerCase());
-                const matchesType = selectedType === 'All' || card.type.includes(selectedType) ;
+                const matchesType = selectedType === 'All' || card.type.includes(selectedType);
                 return matchesSearch && matchesType;
         }).slice(0, 204);
 
@@ -39,26 +39,22 @@ function CardList() {
 
         function shuffleArray(array) {
                 for (let i = array.length - 1; i > 0; i--) {
-                    const j = Math.floor(Math.random() * (i + 1));
-                    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+                        const j = Math.floor(Math.random() * (i + 1));
+                        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
                 }
                 return array;
-            }
+        }
 
-        setTimeout(() => {
-                console.log("->>",[...new Set(Cards.map(item => item.type))]);
-        }, 1000);
         return (
                 <>
-
-                        <div className="app">
-                                <div className="container">
+                        <div className="app container-fluid">
+                                <div className="row">
                                         {/* Left Side: Selected Card */}
-                                        <div className="selected-card">
+                                        <div className="col-md-4 selected-card p-3">
                                                 {selectedCard ? (
                                                         <div>
                                                                 <h2>{selectedCard.name}</h2>
-                                                                <img className="selected-img" src={selectedCard.card_images[0].image_url_small} alt={selectedCard.name} />
+                                                                <img className="selected-img img-fluid" src={selectedCard.card_images[0].image_url_small} alt={selectedCard.name} />
                                                                 <p><strong>Type:</strong> {selectedCard.type}</p>
                                                                 <p><strong>Description:</strong> {selectedCard.desc}</p>
                                                                 <p><strong>TH-Description:</strong> {th_desc(selectedCard.id)}</p>
@@ -67,36 +63,36 @@ function CardList() {
                                                         <p>Select a card to see its details.</p>
                                                 )}
                                         </div>
-                                        <div className="col-md-5">
-
-
-                                        </div>
 
                                         {/* Right Side: Search and Card List */}
-                                        <div className="card-list" >
-                                                <input
-                                                        type="text"
-                                                        placeholder="Search for a card..."
-                                                        value={searchQuery}
-                                                        onChange={(e) => setSearchQuery(e.target.value)}
-                                                />
-                                                <div>
-                                                        <label htmlFor="card-type">Filter by Type: </label>
-                                                        <select id="card-type" value={selectedType} onChange={handleTypeChange}>
-                                                                <option value="All">All</option>
-                                                                <option value="Monster">Monster</option>
-                                                                <option value="Spell">Spell</option>
-                                                                <option value="Trap">Trap</option>
-                                                        </select>
+                                        <div className="col-md-8 card-list">
+                                                <div className="search-filter mb-4">
+                                                        <input
+                                                                type="text"
+                                                                className="form-control mb-3"
+                                                                placeholder="Search for a card..."
+                                                                value={searchQuery}
+                                                                onChange={(e) => setSearchQuery(e.target.value)}
+                                                        />
+                                                        <div>
+                                                                <label htmlFor="card-type" className="mr-2">Filter by Type:</label>
+                                                                <select id="card-type" className="form-select" value={selectedType} onChange={handleTypeChange}>
+                                                                        <option value="All">All</option>
+                                                                        <option value="Monster">Monster</option>
+                                                                        <option value="Spell">Spell</option>
+                                                                        <option value="Trap">Trap</option>
+                                                                </select>
+                                                        </div>
                                                 </div>
-                                                <div className="card-thumbnails">
+
+                                                <div className="card-thumbnails row">
                                                         {filteredCards.map((card) => (
                                                                 <div
                                                                         key={card.id}
                                                                         onClick={() => handleCardSelect(card)}
-                                                                        className="card-thumbnail"
+                                                                        className=" mb-3"
                                                                 >
-                                                                        <img src={card.card_images[0].image_url_small} alt={card.name} />
+                                                                        <img src={card.card_images[0].image_url_small} alt={card.name} className="img-fluid" />
                                                                 </div>
                                                         ))}
                                                 </div>
@@ -104,11 +100,6 @@ function CardList() {
                                 </div>
                         </div>
                 </>
-
-
-
-
-
         );
 }
 
